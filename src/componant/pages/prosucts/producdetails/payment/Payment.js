64eb61ece5d9377b8b5c7a570f9fb8e26b3bf3a7
Alifkhan11 <mdalifk2002@gmail.com>
2024-01-22 16:36:31 +0600

    payment comllet

--- src/componant/pages/prosucts/producdetails/payment/Payment.js
+++ src/componant/pages/prosucts/producdetails/payment/Payment.js
@@ -1,6 +1,17 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import useTitle from '../../../../hocks/usetitle/useTitle';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import Cheakout from './cheakout/Cheakout';
+
+
+
+// const stripePromise = loadStripe(process.env.REACT_APP_TOKEN_STRIP);
+const stripePromise = loadStripe('pk_test_51NrtkkG1p3nVEVTLlIhN9JauWDQ4WVtWQ7GTOSj5wjMyrQjjQ7NMU5KyiRYJ3HKn2xXRQU0D9RLyBbdU5LjkKmSb00fo9FLHfW');
+// console.log(process.env.REACT_APP_TOKEN_STRIP);
+
+
 
 const Payment = () => {
     const dat = useLoaderData()
@@ -14,47 +25,85 @@ const Payment = () => {
         setquentity(quentity - 1)
     }
     const dailyvary = 69
+    const itemtotalprice = (dat.price) * quentity
+    const totalprice=dailyvary + ((dat.price) * quentity)
+    const catdta={
+        dailyvary,
+        itemtotalprice,
+        totalprice,
+        id:dat._id,
+        quentity,
+        img:dat.img,
+        name:dat.name,
+        seller:dat.seller,
+        
+    }
+    const options = {
+        mode: 'payment',
+        amount: totalprice,
+        currency: 'usd',
+        // Fully customizable with appearance API.
+        appearance: {
+            /*...*/
+        },
+    };
+    // console.log(catdta);
     return (
-        <div className='grid grid-cols-3 gap-3 my-8'>
-            {/* user details */}
-            <div className='col-span-2 shadow-xl'>
-                <div className='shadow-xl'>
-                    <h1>user name</h1>
-                    <h1>user phon</h1>
-                    <h1>user address</h1>
-                </div>
-                {/* produc details */}
-                <div className='shadow-xl mt-5'>
-                    <div className='flex shadow-xl'>
-                        <img alt='' className='w-44 rounded-xl' src={dat.img} />
-                        <div className='ml-5 my-2 p-5'>
-                            <h1>{dat.name}</h1>
-                            <h1>  <button disabled={quentity === 1} onClick={ninusehundler} className='mx-4 btn btn-outline'> -</button> {quentity} <button className='mx-4 btn btn-outline' onClick={pleasehendler}>+</button> </h1>
-                            <h1>Quentity : {quentity}</h1>
-                            <h1>Price : {(dat.price)}</h1>
-                            <h1> Total Price : {(dat.price) * quentity}</h1>
-                            <h1>Dalevary chaege : {dailyvary}</h1>
+        <div>
+            <div className='grid grid-cols-3 gap-3 my-8'>
+                {/* user details */}
+                <div className='col-span-2 shadow-xl'>
+                    <div className='shadow-xl'>
+                        <h1>user name</h1>
+                        <h1>user phon</h1>
+                        <h1>user address</h1>
+                        <h1>user Email</h1>
+                    </div>
+                    {/* produc details */}
+                    <div className='shadow-xl mt-5'>
+                        <div className='flex shadow-xl'>
+                            <img alt='' className='w-44 rounded-xl' src={dat.img} />
+                            <div className='ml-5 my-2 p-5'>
+                                <h1>{dat.name}</h1>
+                                <h1>
+                                    <button disabled={quentity === 1} onClick={ninusehundler} className='mx-4 btn btn-outline'> -</button>
+                                    {quentity}
+                                    <button className='mx-4 btn btn-outline' onClick={pleasehendler}>+</button>
+                                </h1>
+                                <h1>Quentity : {quentity}</h1>
+                                <h1>Price : {(dat.price)}</h1>
+                                <h1>Item Total Price : {itemtotalprice}</h1>
+                                <h1>Dalevary chaege : {dailyvary}</h1>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            {/* payment details */}
-            <div className='shadow-xl  divide-y-4 p-5 m-5'>
-                <div className='shadow-xl '>
-                    <p>Discount and Payment</p>
-                    <p className='flex justify-between'><span> Voucher</span><span>setwet</span></p>
-                    <p className='flex justify-between mb-8'><span>Promo Code</span><span>setwet</span></p>
+                {/* payment details */}
+                <div className='shadow-xl  divide-y-4 p-5 m-5'>
+                    <div className='shadow-xl '>
+                        <p>Discount and Payment</p>
+                        <p className='flex justify-between'><span> Voucher</span><span><small>nathing</small></span></p>
+                        <p className='flex justify-between mb-8'><span>Promo Code</span><span><small>nathing</small></span></p>
 
+                    </div>
+                    <div>
+                        <p>Order Summary</p>
+                        <p className='flex justify-between'><span>Item Total Price</span><span>{itemtotalprice}</span></p>
+                        <p className='flex justify-between'><span> Delivery Fee</span><span>{dailyvary}</span></p>
+                        <p className='flex justify-between'><span> Total Payment</span><span>{totalprice}</span></p>
+
+                    </div>
+
+
+                    <div>
+                    </div>
                 </div>
-                <div>
-                    <p>Order Summary</p>
-                    <p className='flex justify-between'><span> Total payment</span><span>{(dat.price) * quentity}</span></p>
-                    <p className='flex justify-between'><span> Delivery Fee</span><span>{dailyvary}</span></p>
-                    <p className='flex justify-between'><span> Total Payment</span><span>{dailyvary + ((dat.price) * quentity)}</span></p>
-
-                </div>
-
             </div>
+                        <Elements stripe={stripePromise} options={options}>
+                            <Cheakout
+                            catdta={catdta}
+                            />
+                        </Elements>
         </div>
     );
 };
--- src/componant/pages/prosucts/producdetails/payment/cheakout/Cheakout.js
+++ src/componant/pages/prosucts/producdetails/payment/cheakout/Cheakout.js
@@ -1,37 +1,144 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Cheakout = () => {
-    const stripe = useStripe();
-    const elements = useElements();
-    const handleSubmit = () => {
+const Cheakout = ({ catdta }) => {
+  const [cardError, setCardError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [processing, setProcessing] = useState(false);
+  const [transactionId, setTransactionId] = useState('');
+  const [clientSecret, setClientSecret] = useState("");
 
+  const stripe = useStripe();
+  const elements = useElements();
+  const { totalprice, name,seller,id,quentity } = catdta;
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:5000/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ totalprice }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, [totalprice]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return
     }
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <CardElement
-                    options={{
-                        style: {
-                            base: {
-                                fontSize: '16px',
-                                color: '#424770',
-                                '::placeholder': {
-                                    color: '#aab7c4',
-                                },
-                            },
-                            invalid: {
-                                color: '#9e2146',
-                            },
-                        },
-                    }}
-                />
-                <button type="submit" disabled={!stripe}>
-                    Pay
-                </button>
-            </form>
-        </div>
+
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+
+    const { error,
+      //  paymentMethod
+       } = await stripe.createPaymentMethod({
+      type: 'card',
+      card
+    });
+
+    if (error) {
+      console.log(error);
+      setCardError(error.message);
+    }
+    else {
+      setCardError('');
+    }
+    setSuccess('');
+    setProcessing(true);
+    const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+      clientSecret,
+      {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: name,
+            // seller,
+            // img:img
+          },
+        },
+      },
     );
+
+    if (confirmError) {
+      setCardError(confirmError.message);
+      return;
+    }
+    console.log(paymentIntent);
+    if (paymentIntent.status === "succeeded") {
+      console.log('card info', card);
+      // store payment info in the database
+      const payment = {
+        price :totalprice,
+        transactionId: paymentIntent.id,
+        seller,
+        orderId: id,
+        quentity
+      }
+      fetch('http://localhost:5000/payments', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(payment)
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          if (data.insertedId) {
+            setSuccess('Congrats! your payment completed');
+            setTransactionId(paymentIntent.id);
+          }
+        })
+    }
+    setProcessing(false);
+  
+
+
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: '16px',
+                color: '#424770',
+                '::placeholder': {
+                  color: '#aab7c4',
+                },
+              },
+              invalid: {
+                color: '#9e2146',
+              },
+            },
+          }}
+        />
+        <button
+          className='btn btn-sm mt-4 btn-primary'
+          type="submit"
+          disabled={!stripe || !clientSecret || processing}>
+          Pay
+        </button>
+      </form>
+      <p className="text-red-500">{cardError}</p>
+      {
+        success && <div>
+          <p className='text-green-500'>{success}</p>
+          <p>Your transactionId: <span className='font-bold'>{transactionId}</span></p>
+        </div>
+      }
+    </>
+  );
 };
 
 export default Cheakout;
